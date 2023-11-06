2989ba813b9c58095739ceb7e48d1338508fe8c9
santoshchappidi11 <santoshchappidi3@gmail.com>
2023-11-06 23:35:29 +0530

    updates

--- facebook/src/components/Posts/SinglePost.js
+++ facebook/src/components/Posts/SinglePost.js
@@ -4,9 +4,13 @@ import { useParams } from "react-router-dom";
 import api from "../../ApiConfig";
 import toast from "react-hot-toast";
 import Navbar from "../Navbar/Navbar";
+import like from "./../../images/like.JPG";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMessage } from "@fortawesome/free-regular-svg-icons";
+import { faShare } from "@fortawesome/free-solid-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+import LikePost from "../Home/LikePost";
 
 const SinglePost = () => {
   const { postId } = useParams();
@@ -89,66 +93,98 @@ const SinglePost = () => {
       <Navbar />
       <div id="single-post">
         <div id="single-post-left">
-          <div id="single-post-header">
-            <div id="single-post-profile-img">
-              <img src={singlePost.userImage} alt="profile" />
-            </div>
-            <h3>
-              {singlePost.userFirstName} {singlePost.userLastName}
-            </h3>
-          </div>
-          <div id="main-post">
-            <div>
-              <div id="single-post-caption">
-                <h4>
-                  Caption : <span>{singlePost?.caption}</span>
-                </h4>
-              </div>
-              <div id="single-post-img">
-                <img src={singlePost?.image} alt="post" />
-              </div>
+          <div id="single-post-img">
+            <div id="main-img">
+              <img src={singlePost?.image} alt="post" />
             </div>
           </div>
         </div>
         <div id="single-post-right">
-          <div id="comment-header">
-            <h3>Comments:</h3>
-          </div>
-
-          {singlePost?.comments?.length ? (
-            singlePost?.comments?.map((item) => (
-              <div id="post-single-comment" key={item.commentId}>
-                <div id="comment-actions">
-                  <div id="edit-comment">
-                    <FontAwesomeIcon
-                      icon={faPenToSquare}
-                      onClick={() => getEditComment(item?.commentId)}
-                    />
-                  </div>
-                  <div id="delete-comment">
-                    <FontAwesomeIcon
-                      icon={faXmark}
-                      onClick={() => deleteComment(item?.commentId)}
-                    />
-                  </div>
+          <div id="single-post-user">
+            <div id="user-up">
+              <div id="post-user">
+                <div id="post-user-img">
+                  <img src={singlePost?.userImage} alt="post-img" />
                 </div>
-                <div id="comment-profile">
-                  <div id="comment-profile-img">
-                    <img src={item.profileImg} alt="profile" />
-                  </div>
-                  <h3>
-                    {item.firstName} {item.lastName}
-                  </h3>
+                <div id="post-user-details">
+                  <h4>
+                    {singlePost?.userFirstName} {singlePost?.userLastName}
+                  </h4>
+                  <p>
+                    2 d · <i class="fa-solid fa-earth-asia"></i>
+                  </p>
                 </div>
-                <div id="main-comment">
-                  <h5>{item.comment}</h5>
-                </div>
-                <p>{editComment}</p>
               </div>
-            ))
-          ) : (
-            <p>No Comments Yet!</p>
-          )}
+              <div className="post-user-options">
+                <i class="fa-solid fa-ellipsis fa-lg"></i>
+              </div>
+            </div>
+            <div id="user-middle">
+              <p>{singlePost?.caption}</p>
+            </div>
+            <div id="user-down">
+              <div id="user-activity">
+                <div id="user-left">
+                  <img src={like} alt="like" />
+                  {/* <p>{singlePost?.likes ? singlePost?.likes?.length : "0"}</p> */}
+                  {/* <img src={heart} alt="heart" /> */}
+                </div>
+                <div id="user-right">
+                  <p>
+                    <FontAwesomeIcon icon={faMessage} /> 531
+                  </p>
+                  <p>
+                    <FontAwesomeIcon icon={faShare} /> 112
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="user-actions">
+            <div>
+              {/* <LikePost
+              postId={post._id}
+              likes={post?.likes}
+              setAllPosts={setAllPosts}
+               />  */}
+              <p>Like</p>
+            </div>
+            <div id="sec-5-comment">
+              <FontAwesomeIcon icon={faMessage} />
+              <p>Comment</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faShare} />
+              <p>Share</p>
+            </div>
+          </div>
+          <div id="single-post-comments">
+            {singlePost ? (
+              singlePost?.comments?.map((item) => (
+                <div className="comment" key={item.commentId}>
+                  <div className="comment-img">
+                    <img src={item.profileImg} alt="comment" />
+                  </div>
+                  <div className="comment-details">
+                    <div className="details">
+                      <h5>
+                        {item.firstName} {item.lastName}
+                      </h5>
+                      <div className="main-comment">
+                        <p>{item.comment}</p>
+                      </div>
+                    </div>
+                    <div className="comment-actions">
+                      <FontAwesomeIcon icon={faPenToSquare} className="edit" />
+                      <FontAwesomeIcon icon={faXmark} className="delete" />
+                    </div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <p>No Comments Yet!</p>
+            )}
+          </div>
         </div>
       </div>
     </>
