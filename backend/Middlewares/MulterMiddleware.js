cfced0ecdafd9d1a72cb0f4bb06f2f22c1bf7290
Santosh_Chappidi <santoshchappidi03@gmail.com>
2024-02-18 16:00:09 +0530

    updates

--- backend/Middlewares/MulterMiddleware.js
+++ backend/Middlewares/MulterMiddleware.js
@@ -1,7 +1,10 @@
 import multer from "multer";
 // import path from "path";
 // import { fileURLToPath } from "url";
+import { fileURLToPath } from "url";
 import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // // Get the current file URL--->
