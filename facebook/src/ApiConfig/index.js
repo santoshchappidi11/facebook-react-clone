ab8ebcd1429b4f25f2a6bc128542265b378a25f7
Santosh_Chappidi <santoshchappidi03@gmail.com>
2024-02-21 19:17:56 +0530

    updates

--- facebook/src/ApiConfig/index.js
+++ facebook/src/ApiConfig/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://facebook-react-clone-ovj4.onrender.com",
+  // baseURL: "http://localhost:8000",
 });
 
 export default api;
